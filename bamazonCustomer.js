c4fc5071857a55be9ca9d332b1dc4040bb9782e2
Dave Bokil <bokild@gmail.com>
2017-05-03 00:29:46 -0400

    finished level 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,38 +14,96 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
 
-    // Your username
+    // username
     user: "root",
 
-    // Your password
+    // password
     password: "",
     database: "Bamazon"
 });
 
-// connect to the mysql server and sql database
+// Connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
 });
 
-// This function will start when app is first run. It serves to a) Show the current product stock, and b) prompt the user to pick a product to purchase
 
+// Function to handle purchasing. 
 var start = function() {
+
+    // When app is run, the most up to date stock of products is displayed:
     connection.query("SELECT * FROM products", function(error, results) {
         if (error) throw error;
+        console.log("\n")
         console.log("==========================")
         console.log("Current Products in Stock:")
         console.log("==========================")
         for (var i = 0; i < results.length; i++) {
             console.log(
-                "Product ID: " + results[i].item_id + "\n" + " | " + "Product: " + results[i].product_name + "\n" + " | " + "Department Name: " + results[i].department_name + "\n" + " | " + "Price: " + "$" + results[i].price + "\n" + " | " + "Quantity: " + results[i].stock_quantity + "\n"
+                results[i].product_name + "\n" + " | " + "Product ID: " + results[i].item_id + "\n" + " | " + "Department Name: " + results[i].department_name + "\n" + " | " + "Price: " + "$" + results[i].price + "\n" + " | " + "Quantity: " + results[i].stock_quantity + "\n"
             );
         }
+
+        // Store the most up to date stock results in an object variable
+        var stock = results
+
+        // Ask the customer the product ID and quantity of items they would like to purchase.
+        inquirer.prompt([{
+                type: 'input',
+                name: 'idPick',
+                message: 'Enter the ID of the product you would like to purchase'
+            }, {
+                type: 'input',
+                name: 'quantity',
+                message: 'How many units would you like to purchase?'
+            }])
+            .then(function(answers) {
+                // store answers in variables
+                var userItem = "";
+                var userNumberItems = answers.quantity;
+                for (var i = 0; i < stock.length; i++) {
+                    if (answers.idPick == stock[i].item_id) {
+                        userItem = stock[i]
+                        // console.log(userItem)
+                    }
+                }
+                // If user wants more quantity than is available, notify, and then restart the app.
+                if (answers.quantity > userItem.stock_quantity) {
+                    console.log("\n" + "We currently dont have that many in stock. Please check back later. " + "Redirecting you to the storefront:")
+                    start();
+
+                // Confirm the Purchase:    
+                } else {
+                    inquirer.prompt([{
+                        type: 'confirm',
+                        name: 'purchaseConfirm',
+                        message: "You would like to purchase " + "(" + answers.quantity + ") " + "of " + "(" + userItem.product_name + ")" + ". " + "Please confirm:"
+                    }]).then(function(answer) {
+                        
+                        // If user confirms, update the SQL stock quantity, show total of purchase, then redirect to the storefront.
+                        if (answer.purchaseConfirm == true) {
+                            connection.query("UPDATE products SET ? WHERE ?", [{
+                              stock_quantity: userItem.stock_quantity - userNumberItems
+                            }, {
+                              item_id: userItem.item_id
+                            }], function(error) {
+                              if (error) throw error;
+                              // show the customer the total cost of their purchase.
+                              console.log("\n" + "Your total is $" + (userItem.price * userNumberItems))
+                              console.log("\n"  +'Congratulations on your purchase. Your item(s) are being processed. Please come back soon!' + "\n"  + "Redirecting to the storefront:")
+                            start();
+                            });        
+
+                        // If user does not confirm purchase, send them back to the storefront.    
+                        } else {
+                            console.log("\n"  + "Looks like you changed your mind. Re-directing to the storefront:" + "\n")
+                            start()
+                        }
+                    })
+                }
+            });
     });
-    // The app should then prompt users with two messages.
-    // The first should ask them the ID of the product they would like to buy.
-    // The second message should ask how many units of the product they would like to buy.
-    connection.end();
 };
 
-
-start()
+// Run the App
+start() 
